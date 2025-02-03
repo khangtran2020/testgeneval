f6a938260d5dd87213ef95b0381d63a4e28e5dac
khangtran2020 <kt36@njit.edu>
2025-02-03 18:28:59 -0500

    parallelizing debug

--- run_llm_analyzer.py
+++ run_llm_analyzer.py
@@ -228,4 +228,5 @@ if __name__ == "__main__":
     parser.add_argument("--temperature", type=float, default=0.2)
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
+    logger.info(f"Running with # processes: {args.num_processes}")
     asyncio.run(main(args))
