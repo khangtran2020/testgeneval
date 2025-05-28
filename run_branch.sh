89ada5ba95142c1f2c7bdd1bd26cff76360c166c
khangtran2020 <kt36@njit.edu>
2025-05-28 10:19:12 -0400

    add run bash file

---
+++ run_branch.sh
@@ -0,0 +1,8 @@
+python run_pipeline_testcase.py --dataset kjain14/testgenevallite \
+    --results_dir ./results/ \
+    --num_processes 16 \
+    --repo all \
+    --data_path "./data" \
+    --glmf_generated_path "fake_generated_sample.json" \
+    --glmf_generated_output "fake_generated_branch.jsonl" \
+    --eval_generated
