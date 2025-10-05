c4a5dda9fa4d714bdc7b15b5afa513f870397a0d
khangtran2020 <kt36@njit.edu>
2025-10-05 00:38:30 -0400

    add run evaluation glmf

--- run_pipeline_testcase.py
+++ run_pipeline_testcase.py
@@ -28,7 +28,7 @@ def main(args):
     os.makedirs(base_dir, exist_ok=True)
 
     log_dir = os.path.join(base_dir, "data_logs", model_suf)
-    print(log_dir)
+    print("Log dirs:", log_dir)
     os.makedirs(log_dir, exist_ok=True)
 
     pred_dir = os.path.join(base_dir, "preds")
