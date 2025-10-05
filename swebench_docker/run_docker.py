0ad32402d3ca3bc8d236c2cb8d8f27e254d03a9f
khangtran2020 <kt36@njit.edu>
2025-10-05 18:57:54 -0400

    debugging

--- swebench_docker/run_docker.py
+++ swebench_docker/run_docker.py
@@ -173,29 +173,31 @@ async def run_docker_evaluation(
             logger.info(
                 f"[{task_instance['id']}][{docker_image}]  Container ran successfully in {elapsed_time} seconds."
             )
-        # read task instance from tmpfile_path
-        if translated == -1:
-            branch_key = "branches"
-        else:
-            branch_key = f"branch_translate_{translated}"
-        if os.path.exists(
-            os.path.join(log_dir, f"{task_instance[KEY_ID]}_setting_{setting}.json")
-        ):
-            with open(
-                os.path.join(
-                    log_dir, f"{task_instance[KEY_ID]}_setting_{setting}.json"
-                ),
-                "r",
-            ) as f:
-                task_instance = json.load(f)
-                logger.info(
-                    f"Task instance {task_instance[KEY_ID]} for setting {setting} loaded from {task_instance[KEY_ID]}_setting_{setting}.json"
-                )
-                logger.info(f"Task instance branches: {task_instance[branch_key]}")
-            return task_instance, setting
-        else:
-            logger.error("task_instance_results.json not found")
-            return task_instance, setting
+
+        if "test_case" in setting:
+            # read task instance from tmpfile_path
+            if translated == -1:
+                branch_key = "branches"
+            else:
+                branch_key = f"branch_translate_{translated}"
+            if os.path.exists(
+                os.path.join(log_dir, f"{task_instance[KEY_ID]}_setting_{setting}.json")
+            ):
+                with open(
+                    os.path.join(
+                        log_dir, f"{task_instance[KEY_ID]}_setting_{setting}.json"
+                    ),
+                    "r",
+                ) as f:
+                    task_instance = json.load(f)
+                    logger.info(
+                        f"Task instance {task_instance[KEY_ID]} for setting {setting} loaded from {task_instance[KEY_ID]}_setting_{setting}.json"
+                    )
+                    logger.info(f"Task instance branches: {task_instance[branch_key]}")
+                return task_instance, setting
+            else:
+                logger.error("task_instance_results.json not found")
+                return task_instance, setting
     except Exception as e:
         logger.warning(
             f"[{task_instance['id']}][{docker_image}]  Error running container: {e}"
